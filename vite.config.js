0d592763f75ca1b7833093c100d7aef43988ee2f
Darshi <darshiu@yahoo.com>
2026-03-29 22:13:03 +0800

    init project

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/movieHub',
+  base: '/',
    server:{
     port: 8086,
     host: true,
